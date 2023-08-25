c30ae3486d54279b52ccc3378b06f06be07bb246
pjcode96 <pjsaez96@gmail.es>
2023-08-25 21:58:18 +0200

    add header

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+import Head from 'next/head'
+import Header from './components/header/header'
+
+
 export default function Home() {
   return (
-    <h1>Hola mundo en next</h1>
+    <></>
   )
 }
---
+++ app/products/page.js
@@ -0,0 +1,7 @@
+function Product() {
+    return (
+        <h1>Hola desde tienda</h1>
+    )
+}
+
+export default Product
